7e855c050791afa84976df23eb1bcf9f03e1fd4a
YASH WASULKAR <145082484+yashkun00@users.noreply.github.com>
2026-09-09 22:32:40 +0530

    a

--- B1.py
+++ B1.py
@@ -1,14 +1,19 @@
-# python fundamentals-1
-
-
 1. variables- The unknown data values present in data sets 
+---
 2. References- address for locating the data in memory 
+---
 3. object references-finding the data with the help of ref
+---
 4. mutable- changable data value
+---
 5. immutable- unchangable data value
+---
 6. list- similar types of data stored in same place
+---
 7. tuples- diff types of data stored in same arry 
+---
 8. stings- collection of charaters
+---
 9. function- a block of instructons stored in one place
         def fun (this is how the function is defined)
 10. parameter- unknow data value
